5ce5cbf3ae61da38401aff08dd4163bd5f2a9be8
Adamauer1 <adamauer1@gmail.com>
2024-12-03 11:56:59 +0100

    adding second solution for leader submission

--- src/app/submit-leader/page.tsx
+++ src/app/submit-leader/page.tsx
@@ -14,6 +14,7 @@ import {
   Text,
   Container,
   Image,
+  RingProgress,
 } from "@mantine/core";
 import { useEffect, useReducer, useRef, useState } from "react";
 import { checkCentury, Leader } from "@/lib/utils";
@@ -26,6 +27,8 @@ import { useForm } from "@mantine/form";
 
 export default function SubmitLeader() {
   const [isLoading, setIsLoading] = useState(false);
+  const [test, setTest] = useState(false);
+  const [value, setValue] = useState(0);
   const [previewValues, setPreviewValues] = useState<formValues>();
   const form = useForm({
     mode: "uncontrolled",
@@ -52,6 +55,29 @@ export default function SubmitLeader() {
 
     onValuesChange: (values) => {
       setPreviewValues(values);
+      let newPercentage = 0;
+      if (values.name != "" || undefined) {
+        newPercentage += 14;
+      }
+      if (values.title != "" || undefined) {
+        newPercentage += 14;
+      }
+      if (values.country != "" || undefined) {
+        newPercentage += 14;
+      }
+      if (values.region != "" || undefined) {
+        newPercentage += 14;
+      }
+      if (values.century != "" || undefined) {
+        newPercentage += 14;
+      }
+      if (values.imageLink != "" || undefined) {
+        newPercentage += 14;
+      }
+      if (values.wikiLink != "" || undefined) {
+        newPercentage += 16;
+      }
+      setValue(newPercentage);
     },
   });
 
@@ -68,6 +94,10 @@ export default function SubmitLeader() {
     continent: previewValues?.region,
     century: previewValues?.century,
   };
+
+  const handleTest = (value: boolean) => {
+    setTest(value);
+  };
   return (
     <>
       {isLoading ? (
@@ -75,122 +105,223 @@ export default function SubmitLeader() {
           <Loader />
         </Center>
       ) : (
-        <Flex
-          direction={{ base: "column", sm: "row" }}
-          justify={"space-around"}
-          align={"center"}
-          flex={1}
-          pt={{ lg: rem(60) }}
-          w={"100%"}
-        >
-          <Flex direction={"column"}>
-            <LeaderFrame
-              name={
-                previewValues?.name == undefined
-                  ? "George Washington"
-                  : previewValues.name
-              }
-              title={
-                previewValues?.title == undefined
-                  ? "President"
-                  : previewValues.title
-              }
-              country={
-                previewValues?.country == undefined
-                  ? "United States of America"
-                  : previewValues.country
-              }
-              image={
-                previewValues?.imageLink == undefined
-                  ? "https://upload.wikimedia.org/wikipedia/commons/b/b6/Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg"
-                  : previewValues.imageLink
-              }
-              link={
-                previewValues?.wikiLink == undefined
-                  ? "https://en.wikipedia.org/wiki/George_Washington"
-                  : previewValues.wikiLink
-              }
-              guessNumber={5}
-              gameOver={true}
-            />
-            <GuessRow
-              id={"1"}
-              key={1}
-              leader={leader}
-              answer={leader}
-              centuries={["green", "\u{02713}"]}
-            />
-          </Flex>
-          <Flex direction={"column"} w={rem(600)}>
-            <form onSubmit={form.onSubmit(handleSubmit)}>
-              <TextInput
-                label="Name"
-                placeholder="George Washington"
-                {...form.getInputProps("name")}
-                classNames={{ input: styles.input, label: styles.inputLabel }}
+        <>
+          <Button className={styles.control} onClick={() => handleTest(false)}>
+            Test 1
+          </Button>
+          <Button className={styles.control} onClick={() => handleTest(true)}>
+            Test 2
+          </Button>
+          {test ? (
+            <Flex
+              direction={{ base: "column", sm: "row" }}
+              justify={"space-around"}
+              align={"center"}
+              flex={1}
+              pt={{ lg: rem(60) }}
+              w={"100%"}
+            >
+              <Flex direction={"column"}>
+                <LeaderFrame
+                  name={
+                    previewValues?.name == undefined
+                      ? "George Washington"
+                      : previewValues.name
+                  }
+                  title={
+                    previewValues?.title == undefined
+                      ? "President"
+                      : previewValues.title
+                  }
+                  country={
+                    previewValues?.country == undefined
+                      ? "United States of America"
+                      : previewValues.country
+                  }
+                  image={
+                    previewValues?.imageLink == undefined
+                      ? "https://upload.wikimedia.org/wikipedia/commons/b/b6/Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg"
+                      : previewValues.imageLink
+                  }
+                  link={
+                    previewValues?.wikiLink == undefined
+                      ? "https://en.wikipedia.org/wiki/George_Washington"
+                      : previewValues.wikiLink
+                  }
+                  guessNumber={5}
+                  gameOver={true}
+                />
+                <GuessRow
+                  id={"1"}
+                  key={1}
+                  leader={leader}
+                  answer={leader}
+                  centuries={["green", "\u{02713}"]}
+                />
+              </Flex>
+              <Flex direction={"column"} w={rem(600)}>
+                <form onSubmit={form.onSubmit(handleSubmit)}>
+                  <TextInput
+                    label="Name"
+                    placeholder="George Washington"
+                    {...form.getInputProps("name")}
+                    classNames={{
+                      input: styles.input,
+                      label: styles.inputLabel,
+                    }}
+                  />
+                  <TextInput
+                    label="Title"
+                    placeholder="President"
+                    {...form.getInputProps("title")}
+                    classNames={{
+                      input: styles.input,
+                      label: styles.inputLabel,
+                    }}
+                  />
+                  <TextInput
+                    label="Country"
+                    placeholder="United States of America"
+                    {...form.getInputProps("country")}
+                    classNames={{
+                      input: styles.input,
+                      label: styles.inputLabel,
+                    }}
+                  />
+                  <TextInput
+                    label="Region"
+                    placeholder="North America"
+                    {...form.getInputProps("region")}
+                    classNames={{
+                      input: styles.input,
+                      label: styles.inputLabel,
+                    }}
+                  />
+                  <TextInput
+                    label="Century"
+                    placeholder="18"
+                    {...form.getInputProps("century")}
+                    classNames={{
+                      input: styles.input,
+                      label: styles.inputLabel,
+                    }}
+                  />
+                  <TextInput
+                    label="Image Link"
+                    placeholder="https://upload.wikimedia.org/wikipedia/commons/b/b6/Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg"
+                    {...form.getInputProps("imageLink")}
+                    classNames={{
+                      input: styles.input,
+                      label: styles.inputLabel,
+                    }}
+                  />
+                  <TextInput
+                    label="Wikipedia Link"
+                    placeholder="https://en.wikipedia.org/wiki/George_Washington"
+                    {...form.getInputProps("wikiLink")}
+                    classNames={{
+                      input: styles.input,
+                      label: styles.inputLabel,
+                    }}
+                  />
+                  <Group justify="center" mt="md">
+                    <Button type="submit" className={styles.control}>
+                      Submit
+                    </Button>
+                  </Group>
+                </form>
+              </Flex>
+            </Flex>
+          ) : (
+            <Flex
+              direction={{ base: "column" }}
+              // justify={"space-around"}
+              gap={rem(30)}
+              align={"center"}
+              flex={1}
+              pt={{ lg: rem(60) }}
+              w={"100%"}
+            >
+              <Flex direction={"column"} w={rem(600)}>
+                <form onSubmit={form.onSubmit(handleSubmit)}>
+                  <TextInput
+                    label="Name"
+                    placeholder="George Washington"
+                    {...form.getInputProps("name")}
+                    classNames={{
+                      input: styles.input,
+                      label: styles.inputLabel,
+                    }}
+                  />
+                  <TextInput
+                    label="Title"
+                    placeholder="President"
+                    {...form.getInputProps("title")}
+                    classNames={{
+                      input: styles.input,
+                      label: styles.inputLabel,
+                    }}
+                  />
+                  <TextInput
+                    label="Country"
+                    placeholder="United States of America"
+                    {...form.getInputProps("country")}
+                    classNames={{
+                      input: styles.input,
+                      label: styles.inputLabel,
+                    }}
+                  />
+                  <TextInput
+                    label="Region"
+                    placeholder="North America"
+                    {...form.getInputProps("region")}
+                    classNames={{
+                      input: styles.input,
+                      label: styles.inputLabel,
+                    }}
+                  />
+                  <TextInput
+                    label="Century"
+                    placeholder="18"
+                    {...form.getInputProps("century")}
+                    classNames={{
+                      input: styles.input,
+                      label: styles.inputLabel,
+                    }}
+                  />
+                  <TextInput
+                    label="Image Link"
+                    placeholder="https://upload.wikimedia.org/wikipedia/commons/b/b6/Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg"
+                    {...form.getInputProps("imageLink")}
+                    classNames={{
+                      input: styles.input,
+                      label: styles.inputLabel,
+                    }}
+                  />
+                  <TextInput
+                    label="Wikipedia Link"
+                    placeholder="https://en.wikipedia.org/wiki/George_Washington"
+                    {...form.getInputProps("wikiLink")}
+                    classNames={{
+                      input: styles.input,
+                      label: styles.inputLabel,
+                    }}
+                  />
+                  <Group justify="center" mt="md">
+                    <Button type="submit" className={styles.control}>
+                      Submit
+                    </Button>
+                  </Group>
+                </form>
+              </Flex>
+              <RingProgress
+                sections={[{ value, color: "blue" }]}
+                transitionDuration={250}
+                label={<Text ta="center">{value}%</Text>}
               />
-              <TextInput
-                label="Title"
-                placeholder="President"
-                {...form.getInputProps("title")}
-                classNames={{ input: styles.input, label: styles.inputLabel }}
-              />
-              <TextInput
-                label="Country"
-                placeholder="United States of America"
-                {...form.getInputProps("country")}
-                classNames={{ input: styles.input, label: styles.inputLabel }}
-              />
-              <TextInput
-                label="Region"
-                placeholder="North America"
-                {...form.getInputProps("region")}
-                classNames={{ input: styles.input, label: styles.inputLabel }}
-              />
-              <TextInput
-                label="Century"
-                placeholder="18"
-                {...form.getInputProps("century")}
-                classNames={{ input: styles.input, label: styles.inputLabel }}
-              />
-              <TextInput
-                label="Image Link"
-                placeholder="https://upload.wikimedia.org/wikipedia/commons/b/b6/Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg"
-                {...form.getInputProps("imageLink")}
-                classNames={{ input: styles.input, label: styles.inputLabel }}
-              />
-              <TextInput
-                label="Wikipedia Link"
-                placeholder="https://en.wikipedia.org/wiki/George_Washington"
-                {...form.getInputProps("wikiLink")}
-                classNames={{ input: styles.input, label: styles.inputLabel }}
-              />
-              <Group justify="center" mt="md">
-                <Button type="submit" className={styles.control}>
-                  Submit
-                </Button>
-              </Group>
-            </form>
-          </Flex>
-          {/* <LeaderFrame
-            name={"George Washington"}
-            title={"President"}
-            country={"United States of America"}
-            image={
-              "https://upload.wikimedia.org/wikipedia/commons/b/b6/Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg"
-            }
-            link={"https://en.wikipedia.org/wiki/George_Washington"}
-            gameOver={true}
-          /> */}
-          {/* <Flex
-            direction={"column"}
-            flex={{ lg: 0.6 }}
-            pt={{ lg: rem(80) }}
-            p={{ base: rem(15) }}
-            align={"center"}
-            gap={{ lg: rem(30) }}
-          ></Flex> */}
-        </Flex>
+            </Flex>
+          )}
+        </>
       )}
     </>
   );
